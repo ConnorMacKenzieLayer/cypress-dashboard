a44c204137343e40a9dae47d09ccd9181daf458a
Connor MacKenzie <connor@layerci.com>
2021-11-10 17:31:21 -0500

    Layerfile, build front end

--- back-end/index.js
+++ back-end/index.js
@@ -1,11 +1,17 @@
+const path = require('path');
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3001
+const FRONT_END_PATH = path.join(__dirname, '..', 'front-end', 'build')
+
+app.use(express.json());
+
+app.use(express.static(FRONT_END_PATH));
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Cypress Dashboard listening at http://localhost:${port}`)
 })
