f383111eee6214980fd29be19185d43a222becce
Connor MacKenzie <connor@layerci.com>
2021-11-24 19:22:32 -0500

    Serve videos

--- back-end/index.js
+++ back-end/index.js
@@ -11,17 +11,30 @@ app.use(express.json());
 
 app.use(express.static(FRONT_END_PATH));
 
-app.use("/video", express.static(__dirname + "/videos"))
+app.get('/:jobUuid/videos', function(req, res, next){
+    let jobUuid = req.params.jobUuid;
+    let src = 'cypress/videos'
+    let dest = `/var/tmp/${jobUuid}/cypress/videos`
+    let videos;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+    copyDirectory(src, dest, jobUuid).then(() => {
+        videos = listVideos(dest);
+    }).finally(() => res.send(videos));
+});
+
+app.get('/:jobUuid/videos/:videoFile', function(req, res, next){
+    let jobUuid = req.params.jobUuid;
+    let file = `/var/tmp/${jobUuid}/cypress/videos/${req.params.videoFile}`
+    res.sendFile(file);
+});
 
 app.get('/:jobUuid/test-list', function(req, res, next){
     let formattedResults = {};
     let jobUuid = req.params.jobUuid;
+    let src = 'cypress/results'
+    let dest = `/var/tmp/${jobUuid}/cypress/results`
 
-    copyDirectory(jobUuid).then(() => {
+    copyDirectory(src, dest, jobUuid).then(() => {
         let testSuiteResults = getTestSuiteResults(`/var/tmp/${jobUuid}/cypress/results/`)
         formattedResults = formatTestSuiteResults(testSuiteResults)
     }).finally(() => res.send(formattedResults));
@@ -30,8 +43,10 @@ app.get('/:jobUuid/test-list', function(req, res, next){
 app.get('/:jobUuid/test-details/:testName', function(req, res, next){
     let formattedResults = {};
     let jobUuid = req.params.jobUuid;
+    let src = 'cypress/results'
+    let dest = `/var/tmp/${jobUuid}/cypress/results`
 
-    copyDirectory(jobUuid).then(() => {
+    copyDirectory(src, dest, jobUuid).then(() => {
         let testSuiteResults = getTestSuiteResults(`/var/tmp/${jobUuid}/cypress/results/`)
         formattedResults = formatTestResults(testSuiteResults, req.params.testName)
     }).finally(() => res.send(formattedResults));
@@ -69,10 +84,16 @@ function getTestSuiteResults(testResultsDirectory) {
     return json;
 }
 
-async function copyDirectory(jobUuid) {
+function listVideos(videoDirectory) {
+    return fs.readdirSync(videoDirectory).map(file => {
+        if(path.extname(file) === ".mp4") {
+            return file;
+        }
+    });
+}
+
+async function copyDirectory(src, dest, jobUuid) {
     const sftp = new Client();
-    let src = 'cypress'
-    let dest = `/var/tmp/${jobUuid}/cypress`
 
      fs.mkdir(dest, {recursive: true}, (err) => {
         if (err) {
@@ -91,10 +112,7 @@ async function copyDirectory(jobUuid) {
             console.log(`Listener: Download ${info.source}`);
         });
 
-        let resultsPromise = sftp.downloadDir(src + "/results", dest + "/results");
-        let videosPromise = sftp.downloadDir(src + "/videos", dest + "/videos");
-
-        let result = await Promise.all([resultsPromise, videosPromise])
+        let result = await sftp.downloadDir(src, dest);
         return result;
     } catch (err) {
         console.error(err);
