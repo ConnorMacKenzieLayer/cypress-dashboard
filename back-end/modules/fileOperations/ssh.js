03c6ea19543ef2dff5162e1cc755f0ff20d496b3
Connor MacKenzie <connor@layerci.com>
2021-11-26 14:17:55 -0500

    fix fetch

--- back-end/modules/fileOperations/ssh.js
+++ back-end/modules/fileOperations/ssh.js
@@ -3,19 +3,21 @@ const fetch = require("node-fetch");
 const fs = require("fs");
 
 const getIpAdress = async (jobUuid) => {
-    fetch(`http://webapplocal.io/pluginapi/job/${jobUuid}/runner_ips`)
+    let result = "";
+    await fetch(`http://webapp.io/pluginapi/job/${jobUuid}/runner_ips`)
         .then(x => x.json())
         .then(runners => {
             runners.forEach(runner => {
                 if(runner["layerfile_path"].includes("cypress")){
-                    return runner["running_pod_ip4"];
+                    result = runner["running_pod_ip4"];
                 }
             })
         })
         .catch(e => {
             console.error(e)
-            return "";
         });
+
+    return result;
 }
 
 const copyDirectory = async (src, dest, jobUuid) => {
